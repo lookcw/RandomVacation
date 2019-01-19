406d5caadc3e19b5ae57f4edad3af0432eb91520
lookcw <clook@terpmail.umd.edu>
2019-01-18 21:51:36 -0500

    fixing input boxes

---
+++ hello-world/src/top-bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HUE from '@material-ui/core/colors/HUE';
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+const color = red[500];
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
